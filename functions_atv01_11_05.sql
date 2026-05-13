4077e4479a8619043af0ee5ce8a2af07e4e8fe34
byFranca <miguel.sou.franca@gmail.com>
2026-05-13 14:35:11 -0300

    ADIÇÃO: atividade 5

--- functions_atv01_11_05.sql
+++ functions_atv01_11_05.sql
@@ -73,3 +73,38 @@ BEGIN
 END$$
 
 DELIMITER ;
+
+-- ATIVIDADE 5
+
+DELIMITER $$
+
+CREATE FUNCTION fn_nivel_cliente(p_customer_id VARCHAR(5))
+RETURNS VARCHAR(20)
+DETERMINISTIC
+BEGIN
+    DECLARE v_total DECIMAL(10, 2);
+    DECLARE v_nivel VARCHAR(20);
+
+    SELECT 
+        SUM(od.UnitPrice * od.Quantity * (1 - od.Discount))
+    INTO v_total
+    FROM Customers c
+    INNER JOIN Orders o ON c.CustomerID = o.CustomerID
+    INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
+    WHERE c.CustomerID = p_customer_id;
+
+    IF v_total > 10000 THEN
+        SET v_nivel = 'PREMIUM';
+    ELSEIF v_total > 5000 THEN
+        SET v_nivel = 'GOLD';
+    ELSEIF v_total > 1000 THEN
+        SET v_nivel = 'SILVER';
+    ELSE
+        SET v_nivel = 'BRONZE';
+    END IF;
+
+    RETURN v_nivel;
+
+END$$
+
+DELIMITER ;
